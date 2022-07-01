c41c5499d0a4a2026a14100ceea41f8a0a4d94b0
Nymeku <arnaud.andrieu@epitech.eu>
2022-07-01 11:23:10 +0200

    repait images

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
 				<div className="background">
 					{images.map((a) => (
 						<div>
-							<img src={a} alt="" />
+							<img src={a} alt={a} />
 						</div>
 					))}
 				</div>
